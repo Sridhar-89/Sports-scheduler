e8b3a4ea7e4fc32cba3dc99a1c05c12e03725c68
Sridhar-89 <sridharbodhanapu89@gmail.com>
2023-06-06 21:13:17 +0530

    modified app.js

--- capstone/app.js
+++ capstone/app.js
@@ -3,9 +3,10 @@ const app = express();
 var csrf = require("tiny-csrf");
 const path = require("path");
 const flash = require("connect-flash");
-const { user,sport } = require("./models");
+const { user,sport,slot } = require("./models");
 var cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
@@ -42,20 +43,24 @@ app.use(
       {
         usernameField: "email",
         passwordField: "password",
+        passReqToCallback:true
       },
-      (username, password, done) => {
+      (req,username, password, done) => {
         user.findOne({ where: { email: username } })
           .then(async function (user) {
             console.log("password", user.password);
             const result = await bcrypt.compare(password, user.password);
+            console.log("body",req.body.role)
+            let role1=req.body.role;
             if (result) {
-                if(user.role=="admin"){
-                    session.role = 'admin'
+                if(user.role===role1){
+                    session.role = role1
                 }
                 else{
-                    session.role = 'player'
+                  console.log("role error")
+                  return done(null, false, { message: "invalid role" });
                 }
-                
+                console.log("session",session.role)
               return done(null, user);
             } else {
               return done(null, false, { message: "Invalid password" });
@@ -166,46 +171,122 @@ app.get("/signup", (request, response) => {
       response.redirect("/");
     });
   });
+
+
   
-  app.get("/admin-dashboard", async(request, response) => {
+app.get(
+  "/allsports",
+  async function (request, response) {
+    try {
+      const allsports=await sport.getallsports();
+      return response.json(allsports);
+    } catch (error) {
+      console.log(error);
+      return response.status(422).json(error);
+    }
+  }
+);
+
+
+  app.get("/admin-dashboard", connectEnsureLogin.ensureLoggedIn(),  async(request, response) => {
     console.log("user",request.user.id);
     const allsports=await sport.getall(request.user.id);
     console.log("sports",allsports)
     response.render("admin-dashboard",{username:request.user.name,userid:request.user.id,allsports:allsports});
   })
 
-  app.get("/player-dashboard", async(request, response) => {
+  app.get("/player-dashboard",connectEnsureLogin.ensureLoggedIn(), async(request, response) => {
     console.log("user",request.user.id);
     const allsports=await sport.getallsports();
     console.log("sports",allsports)
     response.render("player-dashboard",{username:request.user.name,userid:request.user.id,allsports:allsports});
   })
 
-  app.get("/newSport", (request, response) => {
+  app.get("/newSport",connectEnsureLogin.ensureLoggedIn(), (request, response) => {
     //console.log("user",request.user.id);
     response.render("newSport",{userid:request.user.id});
   })
-  app.post("/newSport/:uid", async(request, response) => {
-    //console.log("user",request.user.id);
+  app.post("/newSport",connectEnsureLogin.ensureLoggedIn(), async(request, response) => {
+    console.log("user1",request.user.id);
     const sport1= await sport.create({
         name:request.body.sport,
-        userId:request.params.uid,
+        userId:request.user.id,
     })
     response.redirect("/admin-dashboard");
   })
   
-  app.get("/sport/:sid", async(request, response) => {
+  app.get("/sport/:sid",connectEnsureLogin.ensureLoggedIn(), async(request, response) => {
     console.log("session",session.role);
     console.log("sport",request.params.sid);
     const sports1=await sport.getdetails(request.params.sid);
-    console.log("sports",sports1)
+    console.log("sports",sports1[0])
+
+    const allsessions=await slot.getAll();
+    const previous1=await slot.previous(request.params.sid);
+    const upcoming1=await slot.upcoming(request.params.sid);
+    console.log("upcoming is",upcoming1);
+    console.log("previous is",previous1);
+
+
     if(session.role=="admin"){
-        response.render("sport",{userid:request.user.id,sport:sports1});
+      if (request.accepts("html")) {
+        response.render("sport",{userid:request.user.id,sport:sports1[0],sessions:allsessions,upcoming:upcoming1,previous:previous1});
+
+      }
+      else{
+        response.json({
+          userid:request.user.id,sport:sports1[0],sessions:allsessions,upcoming:upcoming1,previous:previous1
+        })
+      }
     }
     else{
-        response.render("sportsessions",{userid:request.user.id,sport:sports1});
+      if (request.accepts("html")) {
+        response.render("sportsessions",{userid:request.user.id,sport:sports1[0],sessions:allsessions,upcoming:upcoming1,previous:previous1});
+
+      }
+      else{
+        response.json({
+          userid:request.user.id,sport:sports1[0],sessions:allsessions,upcoming:upcoming1,previous:previous1
+        })
+      }
 
     }
+  })
+
+  app.get("/sport/:sid/newSession",async(request,response)=>{
+console.log("session",session.role);
+console.log("user",request.user.id);
+    console.log("sport",request.params.sid);
+    const sports1=await sport.getdetails(request.params.sid);
+    console.log("sports",sports1)
+        response.render("session",{userid:request.user.id,sport:sports1[0]});
+  })
+
+
+  app.post("/sport/:sid/session",async(request,response) =>{
+    const sports1=await sport.getdetails(request.params.sid);
+    console.log("sports",sports1)
+    //response.render("sport",{userid:request.user.id,sport:sports1[0]});
+    const play=request.body.players;
+    const array = play.split(",");
+    console.log(array);
+    const session2= await slot.create({
+      time:request.body.dateandtime,
+      venue:request.body.location,
+      players:array,
+      noofplayers:request.body.noofplayers,
+      userId:request.user.id,
+      sportId:request.params.sid,
+      cancel:false
+    })
+
+
+
+
+    response.redirect(`/sport/${request.params.sid}`)
+  
+    
+    
   })
 
 
